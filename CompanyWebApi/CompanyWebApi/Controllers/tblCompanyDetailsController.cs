65ce4d44acfb74f066c808ee4207b26ae6fe8d92
Aarchu18 <archana.kumari@kelltontech.com>
2018-09-19 23:03:42 +0530

    first commit

---
+++ CompanyWebApi/CompanyWebApi/Controllers/tblCompanyDetailsController.cs
@@ -0,0 +1,122 @@
+﻿using DataAcessLayer;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+//using CompanyWebApi.Models;
+
+namespace CompanyWebApi.Controllers
+{
+    public class tblCompanyDetailsController : ApiController
+    {
+        private CompanyEntities db = new CompanyEntities();
+       
+
+        // GET: api/tblCompanyDetails
+         public IQueryable<tblCompanyDetail> GettblCompanyDetails()
+         {
+            return DAL.GetAllCompanyDetails(); 
+          }
+
+        // GET: api/tblCompanyDetails/5
+         [ResponseType(typeof(tblCompanyDetail))]
+          public IHttpActionResult GettblCompanyDetail(int id)
+          {
+              tblCompanyDetail tblCompanyDetail = DAL.GetCompany(id);
+            if (tblCompanyDetail == null)
+              {
+                  return NotFound();
+              }
+
+              return Ok(tblCompanyDetail);
+          }
+
+          // PUT: api/tblCompanyDetails/5
+          [ResponseType(typeof(void))]
+          public IHttpActionResult PuttblCompanyDetail(int id, tblCompanyDetail tblCompanyDetail)
+          {
+              if (!ModelState.IsValid)
+              {
+                  return BadRequest(ModelState);
+              }
+
+              if (id != tblCompanyDetail.CompanyID)
+              {
+                  return BadRequest();
+              }
+
+              db.Entry(tblCompanyDetail).State = EntityState.Modified;
+
+              try
+              {
+                  db.SaveChanges();
+              }
+              catch (DbUpdateConcurrencyException ex)
+              {
+                  if (!tblCompanyDetailExists(id))
+                  {
+                      return NotFound();
+                  }
+                  else
+                  {
+                      throw ex;
+                  }
+              }
+
+              return StatusCode(HttpStatusCode.NoContent);
+          }
+
+          // POST: api/tblCompanyDetails
+          [ResponseType(typeof(tblCompanyDetail))]
+          public IHttpActionResult PosttblCompanyDetail(tblCompanyDetail tblCompanyDetail)
+          {
+            
+              try
+              {
+                   DAL.AddCompanyDetails(tblCompanyDetail);
+              }
+              catch (DbUpdateException ex)
+              {
+                 
+                      throw ex;
+                  
+              }
+
+              return CreatedAtRoute("DefaultApi", new { id = tblCompanyDetail.CompanyID }, tblCompanyDetail);
+          }
+
+          // DELETE: api/tblCompanyDetails/5
+          [ResponseType(typeof(tblCompanyDetail))]
+          public IHttpActionResult DeletetblCompanyDetail(int id)
+          {
+            
+              tblCompanyDetail tblCompanyDetail =DAL.GetCompany(id);
+              if (tblCompanyDetail == null)
+              {
+                  return NotFound();
+              }
+
+              DAL.DeleteCompanyDetails(id);
+              
+
+              return Ok(tblCompanyDetail);
+          }
+
+          protected override void Dispose(bool disposing)
+          {
+              if (disposing)
+              {
+                  db.Dispose();
+              }
+              base.Dispose(disposing);
+          }
+
+          private bool tblCompanyDetailExists(int id)
+          {
+              return db.tblCompanyDetails.Count(e => e.CompanyID == id) > 0;
+          }
+          
+    }
+}
---
+++ DataAcessLayer/DAL.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+
+namespace DataAcessLayer
+{
+    public static class DAL
+    {
+        static CompanyEntities DbContext;
+        static DAL()
+        {
+            DbContext = new CompanyEntities();
+        }
+
+        //GET All Company Details
+        public static IQueryable<tblCompanyDetail> GetAllCompanyDetails()
+        {
+            return DbContext.tblCompanyDetails; 
+         }
+
+        //GET CompanyID
+        public static tblCompanyDetail GetCompany(int CompanyID)
+        {
+            return DbContext.tblCompanyDetails.Where(p => p.CompanyID == CompanyID).FirstOrDefault();
+        }
+
+
+        //GET All Portfolio Details
+        public static IQueryable<tblPortfolioDetail> GetAllPortfolioDetails()
+        {
+            return DbContext.tblPortfolioDetails;
+        }
+
+        //Add Company Details
+        public static bool AddCompanyDetails(tblCompanyDetail companyDetail)
+        {
+            bool status;
+            try
+            {
+                DbContext.tblCompanyDetails.Add(companyDetail);
+                DbContext.SaveChanges();
+                status = true;
+            }
+              catch (DbUpdateException ex)
+           
+            {
+                status = false;
+                throw ex;
+            }
+            return status;
+        }
+
+        //Add Portfolio Details
+        public static bool AddPortfolioDetails(tblPortfolioDetail portfolioDetail)
+        {
+            bool status;
+            try
+            {
+                DbContext.tblPortfolioDetails.Add(portfolioDetail);
+                DbContext.SaveChanges();
+                status = true;
+            }
+            catch (Exception ex)
+            {
+                status = false;
+            }
+            return status;
+        }
+        //update companyDetails
+        public static bool UpdateCompanyDetails(tblCompanyDetail companyDetail)
+        {
+            bool status;
+            try
+            {
+                tblCompanyDetail prodItem = DbContext.tblCompanyDetails.Where(p => p.CompanyID == companyDetail.CompanyID).FirstOrDefault();
+                if (prodItem != null)
+                {
+                    prodItem.CompanyIndex = companyDetail.CompanyIndex;
+                    prodItem.CompanyName = companyDetail.CompanyName;
+                    prodItem.CompanyAddress = companyDetail.CompanyAddress;
+                    prodItem.CompanyEmail = companyDetail.CompanyEmail;
+                    prodItem.CompanyPhoneNo = companyDetail.CompanyPhoneNo;
+                    prodItem.CompanyContactPerson = companyDetail.CompanyContactPerson;
+                    DbContext.SaveChanges();
+                }
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+        //Delete CompanyDetails
+
+        public static bool DeleteCompanyDetails(int id)
+        {
+            bool status;
+            try
+            {
+                tblCompanyDetail prodItem = DbContext.tblCompanyDetails.Where(p => p.CompanyID == id).FirstOrDefault();
+                if (prodItem != null)
+                {
+                    DbContext.tblCompanyDetails.Remove(prodItem);
+                    DbContext.SaveChanges();
+                }
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+
+    }
+}
